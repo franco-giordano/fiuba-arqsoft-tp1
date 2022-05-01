807159d9cbfeb18e142d5fada172630a633be610
JulianVentura <julianv6004@gmail.com>
2022-05-01 19:17:51 -0300

    Integracion de app con bbox

--- app/app.js
+++ app/app.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const axios = require('axios').default
 const app = express()
 
 const PORT = 8000
 
 app.get('/', (req, res) => {
     res.status(200).send('pong')
+})
+
+app.get('/async', async (req, res) => {
+    axios.get('http://bbox:9090').then( response => {
+        res.status(200).send(`Async response: ${response.data}`)
+    }).catch(e => console.log(`Error: ${e}`))
+})
+
+app.get('/sync', async (req, res) => {
+    axios.get('http://bbox:9091').then( response => {
+        res.status(200).send(`Sync response: ${response.data}`)
+    }).catch(e => console.log(`Error: ${e}`))
 })
 
 app.listen(PORT, () => {
---
+++ reload-nginx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+docker exec tp1_nginx_1 /etc/init.d/nginx reload
+
+
+
+
